88e2afb6811540a612630307cf9bdebe97f9aaba
danieltanyi <tanyi_daniel@yahoo.com>
2022-07-21 10:07:07 +0200

    modifying class helper

--- src/daniel/utils/Helper.java
+++ src/daniel/utils/Helper.java
@@ -1,4 +1,21 @@
 package daniel.utils;
 
+
+import daniel.entities.TrackInfo;
+
+/**
+ * <h2>Helper</h2>
+ * The utility class used to segregate the system wide resources like clear screen , print, play songs
+ * and displaying artwork image
+ */
 public class Helper {
+    /**
+     * <h2>playSong</h2>
+     *
+     * @param trackInfo provided as parameter to show the artwork image and playing music
+     * @throws Exception throws exception in case when no media player could be found from system registry
+     */
+    public static void playSong(TrackInfo trackInfo) throws Exception {
+
+    }
 }
